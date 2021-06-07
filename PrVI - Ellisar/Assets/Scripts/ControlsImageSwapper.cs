654f43a271f9b2fcdcaaff24951f4f014aee7bb7
Jota <jotagamedev@gmail.com>
2021-06-07 22:35:42 +0200

    Controls panel done

---
+++ PrVI - Ellisar/Assets/Scripts/ControlsImageSwapper.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsImageSwapper : MonoBehaviour
+{
+    [SerializeField] private Sprite gamepadControls, keyboardControls;
+    private UnityEngine.UI.Image image;
+
+    private void Start()
+    {
+        image = GetComponent<UnityEngine.UI.Image>();
+    }
+
+    public void ChangeControlsText(UnityEngine.InputSystem.PlayerInput input)
+    {
+        if (input.currentControlScheme == "Gamepad")
+        {
+            image.sprite = gamepadControls;
+        }
+        else
+        {
+            image.sprite = keyboardControls;
+        }
+    }
+}
